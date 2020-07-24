728253370c1da2f092ed85a1580c3c1b1fe10a41
Nest1k <konstanz.satyr@gmail.com>
2020-07-24 18:30:56 +0300

    выпадающие сообщение

--- js/gallery.js
+++ js/gallery.js
@@ -1,45 +1,56 @@
 'use strict';
 (function () {
-  var thumbnailes = window.picture.pictures.querySelectorAll('.picture');
+  var bigPhoto = function (data) {
+    var thumbnailes = window.picture.pictures.querySelectorAll('.picture');
 
-  var openBigPhoto = function () {
-    window.preview.bigPicture.classList.remove('hidden');
-    document.querySelector('body').classList.add('modal-open');
+    var openBigPhoto = function () {
+      window.preview.bigPicture.classList.remove('hidden');
+      document.querySelector('body').classList.add('modal-open');
 
-    document.addEventListener('keydown', onBigPhotoEscPress);
-  };
+      document.addEventListener('keydown', onBigPhotoEscPress);
+    };
 
-  var addClickHandler = function (thumbnail, photo) {
-    thumbnail.addEventListener('click', function () {
-      window.preview.createBigPicture(photo);
-      //window.preview.createCommentsPool(photo);
-      openBigPhoto();
+    var addClickHandler = function (thumbnail, photo) {
+      thumbnail.addEventListener('click', function () {
+        window.preview.createBigPicture(photo);
+        window.preview.createCommentsPool(photo);
+        openBigPhoto();
+      });
+    };
+
+    for (var i = 0; i < thumbnailes.length; i++) {
+      addClickHandler(thumbnailes[i], data[i]);
+    }
+
+    var bigPhotoCancel = document.querySelector('#picture-cancel');
+
+    var closeBigPhoto = function () {
+      window.preview.bigPicture.classList.add('hidden');
+      document.querySelector('body').classList.remove('modal-open');
+      window.preview.bigСommentsList.innerHTML = ' ';
+
+      document.removeEventListener('keydown', onBigPhotoEscPress);
+    };
+
+    var openBigPhoto = function () {
+      window.preview.bigPicture.classList.remove('hidden');
+      document.querySelector('body').classList.add('modal-open');
+
+      document.addEventListener('keydown', onBigPhotoEscPress);
+    };
+
+    var onBigPhotoEscPress = function (evt) {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        closeBigPhoto();
+      }
+    };
+
+    bigPhotoCancel.addEventListener('click', function () {
+      closeBigPhoto();
     });
   };
 
-  for (var i = 0; i < thumbnailes.length; i++) {
-    addClickHandler(thumbnailes[i], window.data.massUsers[i]);
-  }
-
-  var bigPhotoCancel = document.querySelector('#picture-cancel');
-
-  var closeBigPhoto = function () {
-    window.preview.bigPicture.classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
-    window.preview.bigСommentsList.innerHTML = ' ';
-
-    document.removeEventListener('keydown', onBigPhotoEscPress);
-  };
-
-  var onBigPhotoEscPress = function (evt) {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      closeBigPhoto();
-    }
-  };
-
-  bigPhotoCancel.addEventListener('click', function () {
-    closeBigPhoto();
-  });
+  window.backend.load(bigPhoto);
 
 })();
